1b8b5552aef3a801db71ec4e38720afe13f71c94
SUMIT SHARMA <sumitsharma@192.168.1.14>
2025-09-11 04:16:33 +0530

    Add Google Analytics tracking code to website header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -77,6 +77,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-590658811"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-590658811');
+            `,
+          }}
+        />
+      </head>
       <body className={roboto.className} suppressHydrationWarning={true}>
         <Providers>{children}</Providers>
       </body>
